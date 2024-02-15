375224f39f2709e435d07ccd1002a444193b83c9
Seveyus <152019839+Seveyus@users.noreply.github.com>
2024-02-15 14:11:16 +0100

    Add files via upload

---
+++ DeuxiemeWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+//using System.Windows;
+
+//namespace Application_moodle
+//{
+//    public partial class AutreInterface : Window
+//    {
+//        public AutreInterface()
+//        {
+//            InitializeComponent();
+//        }
+//    }
+//}
+using System.Windows;
+
+namespace Application_moodle
+{
+    public partial class DeuxiemeWindow : Window
+    {
+        public DeuxiemeWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void BtnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Le bouton a été cliqué !"); 
+        }
+    }
+}
