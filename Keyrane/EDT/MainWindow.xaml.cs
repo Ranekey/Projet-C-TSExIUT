596bc70f325a081524dde353da70d8fc492b3065
Ranekey <78801061+Ranekey@users.noreply.github.com>
2024-04-04 12:45:05 +0200

    Update MainWindow.xaml.cs

--- Keyrane/EDT/MainWindow.xaml.cs
+++ Keyrane/EDT/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using EDT_TSE_2.Class;
+using EDT_TSE_2.Class;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
@@ -10,24 +10,38 @@ namespace EDT_TSE_2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
+    
     public partial class MainWindow : INotifyPropertyChanged
     {
         EDT_Manager edt_manager;
         Date_Manager date_manager;
+        
+
         public MainWindow()
         {
+             
+            
             DataContext = this;
             InitializeComponent();
             date_manager = new Date_Manager(ListeSemaines);
+            
+             
+
             edt_manager = new EDT_Manager(grid2, date_manager.current_week_number);
-            Semaine_Correspondance.Text = date_manager.Current_Day.Date.ToString();
+
+
+
+            Semaine_Correspondance.Text = edt_manager.Set_Semaine(date_manager.current_week_number);
+
 
             //SelectWeek.ItemsSource = date_manager.Get_Window_Of_Week(date_manager.week_number); 
         }
 
 
         private int _joursSemaine;
-        public int joursSemaine { get { return _joursSemaine; } set { _joursSemaine = value; } }
+        public int joursSemaine { get { return _joursSemaine; } set {  _joursSemaine = value; } }
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -35,20 +49,25 @@ namespace EDT_TSE_2
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
+        
         private void ListeSemaines_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             int week_selected = int.Parse(e.AddedItems[0].ToString());
+
+
             date_manager.Update_Date(week_selected, ListeSemaines);
             edt_manager.peupler_edt(week_selected, grid2);
+            Semaine_Correspondance.Text = edt_manager.Set_Semaine(week_selected);
             OnPropertyChanged();
         }
 
 
-    }
+
+            
+           
+                
+        }
+        
+
+
 }
